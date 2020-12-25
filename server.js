3374da8da606ad633442313a6cfb5daa2dbe68ce
Serhii Stepanchuk <serjukst@gmail.com>
2020-12-25 21:32:00 +0200

    fix package.json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/task-tracker"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/task-tracker/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/task-tracker"));
 });
 
 app.listen(process.env.PORT || 8080);
