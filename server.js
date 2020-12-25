f24b25779267d5de6da85fe30338d126df47cdb3
Serhii Stepanchuk <serjukst@gmail.com>
2020-12-25 21:07:19 +0200

    fix error in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/task-tracker"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/task-tracker"));
+  res.sendFile(path.join(__dirname + "/dist/task-tracker/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
